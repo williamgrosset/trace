f2bb3022fc28424a90546f18eb0034fab40c495d
William Grosset <williamhgrosset@gmail.com>
2018-03-21 22:44:06 -0700

    Print results function

--- main.py
+++ main.py
@@ -348,6 +348,19 @@ PROTOCOL_TYPE = {
 
 protocol_types = set([])
 
+def print_results(addresses, protocols, round_trip_times):
+    print('The IP address of the source node:')
+    print('The IP address of ultimate destination node:')
+    print('The IP addresses of the intermediate destination nodes:')
+
+    print('The values in the protocol field of IP headers:')
+
+    print('The number of fragments created from the original datagram is:')
+
+    print('The offset of the last fragment is:')
+
+    print('The avg RTT between IP HERE and IP HERE is: VALUE ms, the s.d. is: VALUE ms')
+
 def receive_packets(header, data):
     decoder = ImpactDecoder.EthDecoder()
     ethernet_packet = decoder.decode(data)
